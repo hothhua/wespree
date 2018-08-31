726283a547810d64d0ec22fb55fd51e1d674b56d
hothhua <hothhua@qq.com>
2018-08-31 21:49:43 +0800

    目录错误

---
+++ src/app/core/services/json-api-parser.service.ts
@@ -0,0 +1,16 @@
+import { CJsonApi } from './../models/jsonapi';
+
+export class JsonApiParserService {
+
+constructor() { }
+
+  parseSingleObj(data): Object {
+    return Object.assign(new CJsonApi, data).toModel()
+  }
+
+  parseArrayofObject(array): Array<Object> {
+    return array.map(
+      (obj) => Object.assign(new CJsonApi, obj).toModel()
+    );
+  }
+}
