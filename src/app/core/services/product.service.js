5d472461bb04583d0bc5a9b697f8411a8c585b2e
liuhua <hothhua@qq.com>
2018-09-01 17:44:57 +0800

    测试service

--- src/app/core/services/product.service.js
+++ src/app/core/services/product.service.js
@@ -1,6 +1,19 @@
+import { wxRequest } from './../utils/wxRequest'
+
 import { wepy } from 'wepy'
 
 export default class ProductService {
 
+    getProducts(pageNumber) {
+        let prodApiEndpoint= 'https://ngspree-api.herokuapp.com/'
+
+        let apiUrl = prodApiEndpoint + 'api/v1/products?page='+pageNumber+
+        '&per_page=20&data_set=small'
+
+        const json = wxRequest(null, apiUrl);
+        console.log(json.data);
+
+    }
 
 }
+
